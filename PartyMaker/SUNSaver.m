7ce63a9598af05a6c0b2bb63ef256f01a3290cd8
Tony Star <sunn1vun@gmail.com>
2016-02-09 16:37:04 +0200

    HTLecture 5 for Misha without external comments.

--- PartyMaker/SUNSaver.m
+++ PartyMaker/SUNSaver.m
@@ -23,51 +23,10 @@
 
 -(void)save{
     NSFileManager *filemanager = [NSFileManager defaultManager];
-    NSString *doucumentPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)firstObject];
+    NSString *documentPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES)firstObject];
     
-    NSURL *bundleURL = [[NSBundle mainBundle] bundleURL];
-    
-//    URL for my file in Bundle
-//    NSURL *myLogsURL = [NSURL URLWithString:@"Application/58542917-CDEC-4C8A-B46F-8B81E6DF22BE/PartyMaker.app/myLogs.plist"];
-    NSString *myLogsPath = [[NSBundle mainBundle] resourcePath];
-//    [[[NSBundle mainBundle]resourcePath] stringByAppendingString:@"/myLogs.plist"];
-    
-    NSLog(@"%@", myLogsPath);
-    
-    myLogsPath = [[[NSBundle mainBundle]resourcePath] stringByAppendingString:@"/myLogs.plist"];
-
-    NSLog(@"%@", myLogsPath);
-    
-    NSError *error = nil;
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-    NSArray *contents = [fileManager contentsOfDirectoryAtURL:bundleURL includingPropertiesForKeys:@[] options:NSDirectoryEnumerationSkipsHiddenFiles error:&error];
-    NSLog(@"All nonhiden content of bundle \n%@", contents);
-
-    if(error){
-        NSLog(@"%s NSFileManager error: %@", __PRETTY_FUNCTION__, error);
-    }
-    
-    //        //Path для моего плиста в документах
-    //        NSString *filePath = [documentsPath stringByAppendingString:@"myLogs.plist"];
-    NSString *partiesFolderPath = [doucumentPath stringByAppendingPathComponent: @"logs"];
-    NSString *partiesFilePath = [doucumentPath stringByAppendingPathComponent:@"logs/myLogs.plist"];
-
-    
-    
-//    NSString *partiesFilePathInBundle = [[[NSBundle mainBundle]resourcePath] stringByAppendingString:@"myLogs.plist"];
-//    NSString *partiesFilePathInBundle = [[[NSBundle mainBundle]resourcePath] stringByAppendingString:@"Application/58542917-CDEC-4C8A-B46F-8B81E6DF22BE/PartyMaker.app/myLogs.plist"];
-    NSString *partiesFilePathInBundle = myLogsPath;
-    
-    if( [filemanager fileExistsAtPath:partiesFilePathInBundle] ){
-        
-        NSLog(@"YES");
-    
-    }
-    
-    if ( [filemanager fileExistsAtPath: partiesFolderPath] ) {
-        NSLog(@"Your Party Log exists at documents");
-        NSLog(@"%@", partiesFilePath);
-    }
+    NSString *partiesFilePath = [documentPath stringByAppendingPathComponent:@"logs/myLogs.plist"];
+    NSString *partiesFilePathInBundle = [[[NSBundle mainBundle]resourcePath] stringByAppendingString:@"/myLogs.plist"];
     
     if(![filemanager fileExistsAtPath: partiesFilePath]){
         [filemanager createFileAtPath:partiesFilePath contents:[[NSData alloc] init] attributes:nil];
